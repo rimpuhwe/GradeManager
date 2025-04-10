a0b400a4a01eb25d1532d672edc12713f9bda612
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-10 20:20:03 +0200

    adding the marks together in array

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -1,3 +1,15 @@
+import java.util.Arrays;
+
 public class GradeManager {
+    private double[] marks;
+    //constructor
+    GradeManager(double...marks){
+        this.marks = marks;
+    }
+    //adding grades functionality
+    public  void sumOfMarks(){
+        double sum = Arrays.stream(marks).reduce(0,(a,b) -> a + b);
+        System.out.println("The total of marks: " + sum);
+    }
 
 }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        GradeManager scores = new GradeManager(1,3,4,2,5);
+        scores.sumOfMarks();
+    }
+}
